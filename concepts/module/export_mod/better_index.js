a52cb9cd759223c883d0d10bbde1c50d5573ae40
Nitin Narang <narangnitin520@gmail.com>
2023-08-04 09:24:21 +0530

    Added concepts and multiple_js

---
+++ concepts/module/export_mod/better_index.js
@@ -0,0 +1,8 @@
+const add = require ("./add");
+
+console.log("Hello from better.js");
+
+const sum = add(1,2);
+const sum1= add(7,9);
+console.log(sum);
+console.log(sum1);
---
+++ concepts/module/local_mod/index.js
@@ -0,0 +1,8 @@
+console.log("Hello form index.js");
+
+const add = (a,b) =>{
+    return a+b;
+};
+
+const sum = add(1,2);
+console.log(sum);
